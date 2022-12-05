2d86c1f6d474b885a6b353609c99ec056de3a21e
hdani18 <daniharshil1800@gmail.com>
2022-12-05 02:43:07 -0500

    helper functions

--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
-// Helper functions for server side iput validation 
+// Functions for server side input validation
 
 function checkEmail(email){
     //code for function checkUsername
@@ -31,8 +31,131 @@ function checkPassword(password){
 
 }
 
+function checkFnameLname(name){
+    let regex =/^[a-zA-Z]{2}$/
+    if(regex.test(name) == false){
+        throw 'Enter a valid name';
+    }
+}
+function checkleapYear(year)
+{
+  if ((year % 4 == 0) && (year != 100) || (year == 400)){
+      return true;
+  }
+  else{
+      return false;
+  }
+}
+
+
+function checkDOB(date){
+    let regex = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/
+    if (typeof date != 'string' || !regex.test(date)){
+       throw 'Enter Date in valid format i.e mm/dd/yyyy';
+    }
+    
+    let todayDate = new Date();
+    GivenDate = new Date(date);
+
+
+    if(GivenDate > todayDate){
+        throw 'Given date is greater than the current date';
+    }
+
+    let currentDate = date.split('/');
+
+    if(currentDate.length != 3){
+        return false;
+    }
+    
+    let year = parseInt(currentDate[2]);
+    if(((new Date().getFullYear())- year) >= 12 == false){
+        throw 'You need to atleast be 12 years old';
+    }
+    if(year > new Date().getFullYear()){
+        throw 'You need to be borned first :)';
+    }
+    let month = parseInt(currentDate[0]);
+    console.log
+    if(month < 1 || month > 12 ){
+        throw 'Invalid Month';
+    }
+    let daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+    let days = parseInt(currentDate[1]);
+    if(month == 2){
+        if(checkleapYear(year)){
+            if(days < 1 || days > daysInMonth[1]+1){
+                throw 'Invalid day feb leap year';
+            }
+        } else if(days < 1 || days > daysInMonth[1]){
+            throw 'Invalid day feb';
+        }
+    } else if(days < 1 || days > daysInMonth[month-1]){
+        throw 'Invalid day';
+    }
+    return true;
+}
+function checkBudget(value){
+    if(typeof(value) !== 'string'){
+        throw 'Invalid Budget';
+    }
+    if(value.length == 0){
+        throw 'Invalid Budget';
+    }
+    
+    if(/^([0-9]{1,})$/.test(value) && (Number.isInteger(Number(value)) && Number(value) > 0 )){
+        return true;
+    }
+    throw 'Invalid Budget';
+}
+function checkUserId(userid){
+    if(!Array.isArray(userid)){
+        throw 'userID must me an array only';
+    }
+}
+function checkAmount(amount){
+    if(typeof(amount) !== 'string'){
+        throw 'Invalid Budget';
+    }
+    if(amount.length == 0){
+        throw 'Invalid Budget';
+    }
+    if(/^([0-9]{1,}.{0,1}[0-9]{0,})$/.test(amount)  && Number(amount) > 0){
+        return true;
+    }
+    throw 'Invalid Amount';
+}
+
+function checkTransactionName(name){
+   
+    if(typeof name != 'string' || name.trim().length == 0){
+        throw 'Invalid Transaction Name';
+    }
+    return true;
+}
+function checkGroupName(name){
+    if(typeof name != 'string' || name.trim().length < 2){
+        throw 'Invalid Group Name';
+    }
+    return true;
+}
+function checkObjectId(id){
+    id = id.trim();
+    if(!ObjectId.isValid(id)){
+        throw new Error("Invalid id");
+      }
+}
 
 module.exports = {
     checkEmail,
-    checkPassword
+    checkPassword,
+    checkAmount,
+    checkBudget,
+    checkDOB,
+    checkFnameLname,
+    checkGroupName,
+    checkTransactionName,
+    checkUserId,
+    checkObjectId
 }
+
