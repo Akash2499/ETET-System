cc9665213535a3a467909350768fc62fe8e17fdf
Akash Patel <akash1999patel@gmail.com>
2022-12-05 02:45:33 -0500

    User Data functions added

--- data/user.js
+++ data/user.js
@@ -4,26 +4,210 @@ const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
 const helper = require('../helper');
 const bcryptjs = require('bcryptjs');
+const { ObjectID } = require('bson');
 const saltRounds = 16;
 const transactions = require('./transactions')
 
 
-async function createUser(all_user_related_arguments){
+async function getAllUsers(){
+    const userCollection = await users();
+    const users = await userCollection.find({}).toArray();
+    if (!users){
+    throw 'No users found!';
+    }
+    return users;
+}
+
+
+async function createUser(
+    firstName,
+    lastName,
+    dateOfBirth,
+    email,
+    password,
+    budget
+){
     //will create a user into the database
     //inputchecking should be done 
+
+    email = email.trim().toLowerCase();
+    const userCollection = await users();
+    const userPresent = await  userCollection.findOne({email: email})
+    if(userPresent !== null){
+        throw 'Email already registred'
+    }
+    
+    const passwordHash = await bcryptjs.hash(password,saltRounds)
+
+    const newUser = {
+        firstName: firstName.trim().toLowerCase(),
+        lastName: lastName.trim().toLowerCase(),
+        dateOfBirth: dateOfBirth,
+        email: email.trim().toLowerCase(),
+        password: passwordHash,
+        friends: [],
+        transactions: [],
+        groups: [],
+        budget: budget
+    }
+
+    const insertUser = await userCollection.insertOne(newUser);
+    if (!insertUser.acknowledged || !insertUser.insertedId)
+    throw 'Could not add User';
+
+    return await getUserDetails(insertUser.insertedId.toString())
 }
 
 async function checkUser(email,password){
     //check is user exists in the database
+    //input validation here
+
+    email = email.trim().toLowerCase();
+    password = password.trim();
+
+    const userCollection = await users();
+    const userPresent = await userCollection.findOne({email: email});
+    if(userPresent === null){
+    throw 'Either the email or password is invalid'
+    }
+
+    const passwordCompare = await bcryptjs.compare(password,userPresent.password);
+    if(passwordCompare){
+    return {authenticatedUser: true}
+    }
+    throw 'Either the email or password is invalid'
+}
+
+async function deleteGroupFromUser(userId,groupId){
+    let currentUser = await getUserDetails(userId);
+    let newGroups = []
+    for(let i=0; i<currentUser.groups.length;i++){
+        if(currentUser.groups[i].toString() != groupId){
+            newGroups.push(currentUser.groups[i]);
+        }
+    }
+    const updatedUser = {
+        groups: newGroups
+    }
+
+    const info = await userCollection.updateOne(
+        {_id: ObjectId(userId)},
+        {$set: updatedUser}
+      );
+      if(info.modifiedCount === 0){
+        throw new Error('Cannot update User');
+      }
+    return await getUserDetails(userId)
+}
+
+
+async function updateUser(
+    userId,
+    firstName,
+    lastName,
+    dateOfBirth,
+    budget
+){
+
+
+    
+    const updatedUser = {
+        firstName: firstName.trim().toLowerCase(),
+        lastName: lastName.trim().toLowerCase(),
+        dateOfBirth: dateOfBirth,
+        budget: budget
+    }
+
+    const info = await userCollection.updateOne(
+        {_id: ObjectId(userId)},
+        {$set: updatedUser}
+      );
+      if(info.modifiedCount === 0){
+        throw new Error('Cannot update User');
+      }
+    return await getUserDetails(userId)
 }
 
 
 async function getUserDetails(userId){
     //function to get user details
+
+    userId = userId.trim();
+    const userCollection = await users();
+    const userPresent = await userCollection.findOne({_id: ObjectID(userId)});
+    if(userPresent === null){
+    throw 'Either the email or password is invalid'
+    }
+    userPresent._id = userPresent._id.toString();
+    return userPresent
 }
 
 async function findUserByName(name){
     //returns list of users
+
+    name = name.trim().toLowerCase();
+    const allUsers = await getAllUsers();
+    let searchResult = [];
+    for(let i =0; i<allUsers.length;i++){
+        if(allUsers[i].firstName.includes(name) || allUsers[i].lastName.includes(name)){
+            searchResult.push(allUsers[i]);
+        }
+    }
+
+    if(searchResult == []){
+        throw 'No result found'
+    }
+    return searchResult
+
+}
+
+async function addTransactionToUser(userId,transactionId){
+    let currentUser = await getUserDetails(userId);
+    currentUser.transactions.push(ObjectID(transactionId))
+    const updatedUser = {
+        transactions: currentUser.transactions
+    }
+
+    const info = await userCollection.updateOne(
+        {_id: ObjectId(userId)},
+        {$set: updatedUser}
+      );
+      if(info.modifiedCount === 0){
+        throw new Error('Cannot update User');
+      }
+    return await getUserDetails(userId)
+}
+
+async function deleteTransactionOfUser(userId,transactionId){
+    let currentUser = await getUserDetails(userId);
+    let newTransactions = []
+    for(let i=0; i<currentUser.transactions.length;i++){
+        if(currentUser.transactions[i].toString() != transactionId){
+            newTransactions.push(currentUser.transactions[i]);
+        }
+    }
+    const updatedUser = {
+        transactions: newTransactions
+    }
+
+    const info = await userCollection.updateOne(
+        {_id: ObjectId(userId)},
+        {$set: updatedUser}
+      );
+      if(info.modifiedCount === 0){
+        throw new Error('Cannot update User');
+      }
+    return await getUserDetails(userId)
+}
+
+async function deleteUser(userId){
+    const userCollection = await users();
+    const ans = await getUserDetails(userId);
+    const user = await userCollection.deleteOne({_id: ObjectId(userId)});
+    if (user.deletedCount === 0) {
+    throw new Error('Could not delete User!');
+    }
+    return {deletedUser: true}
 }
 
 const getUserTransactions = async (userId) => {
@@ -60,5 +244,12 @@ module.exports = {
     getUserDetails,
     getUserTransactions,
     getUserTransactionsByMonth,
-    getUserTransactionsByCategory
+    getUserTransactionsByCategory,
+    findUserByName,
+    getAllUsers,
+    updateUser,
+    deleteGroupFromUser,
+    deleteUser,
+    addTransactionToUser,
+    deleteTransactionOfUser
 }
