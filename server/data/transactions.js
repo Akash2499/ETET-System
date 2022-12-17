8f7788361513b21896438958bd0e0b0001efdf7d
kushjani297 <kushjani297@gmail.com>
2022-12-16 19:44:37 -0500

    Added friend and group transaction page and logic

--- server/data/transactions.js
+++ server/data/transactions.js
@@ -21,9 +21,9 @@ const addTransaction = async (
 ) => {
 
     //helper.checkUserId(userIds)
-    userIds = userIds.map((uid)=>{
-        helper.checkObjectId(uid)
-        return ObjectId(uid.trim())
+    userIds.map((uid)=>{
+        helper.checkObjectId(uid.userId)
+        uid.userId.trim()
     })
     helper.checkString(name)
     helper.checkObjectId(paidBy)
@@ -51,8 +51,8 @@ const addTransaction = async (
     if (!insertInfo.acknowledged || !insertInfo.insertedId) throw 'Error while adding transaction'
 
     //await user.addTransactionToUser(paidBy, insertInfo.insertedId.toString())
-    userIds.map(async (userId)=>{
-        await user.addTransactionToUser(userId.toString(), insertInfo.insertedId.toString())
+    userIds.map(async (u)=>{
+        await user.addTransactionToUser(u.userId.toString(), insertInfo.insertedId.toString())
     })
 
     return { inserted : true };
