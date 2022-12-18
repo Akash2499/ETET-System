7c15ca2accca631d232924154eb491cd56b9c701
kushjani297 <kushjani297@gmail.com>
2022-12-18 06:14:19 -0500

    Worked on homepage

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 var cors = require("cors");
 const app = express();
-<<<<<<< HEAD
 const session = require('express-session');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
@@ -16,54 +15,6 @@ var corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(express.json());
-=======
-const session = require("express-session");
-const configRoutes = require("./routes");
-const exphbs = require("express-handlebars");
-
-app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-// const static = express.static(__dirname + '/public');
-// app.use('/public', static);
-
-// app.use(session({
-//     name: 'AuthCookie',
-//     secret: 'This cookie is used to determine login info',
-//     resave: false,
-//     saveUninitialized: true
-//   }));
-
-// app.use('/login', (req,res, next) => {
-//     if(req.session.user){
-//         return res.redirect('/dashboard')
-//     }
-//     else{
-//         req.method = 'POST';
-//         next();
-//     }
-// })
-
-// app.use('/register', (req,res, next) => {
-//     if(req.session.user){
-//         return res.redirect('/dashboard')
-//     }
-//     else{
-//         next();
-//     }
-// })
-
-// app.use('/dashboard', (req,res, next) => {
-//     if(!req.session.user){
-//         return res.redirect('/')
-//     }
-//     else{
-//         next();
-//     }
-// })
->>>>>>> 4a3bb2af69f832fa85eaac8b11254bf00765c69e
 
 configRoutes(app);
 
