a619472d597160c5a3c9360b2527b4b0dc30f89d
patelkahani <patelkahani185@gmail.com>
2022-12-18 01:57:20 -0500

    Added comment component

--- frontend/src/components/Nabvar/Navbar.js
+++ frontend/src/components/Nabvar/Navbar.js
@@ -10,12 +10,40 @@ function Navbar(props) {
 		window.location.href = "/"
 	}
 
+	const addTransaction = (event) => {
+		event.preventDefault()
+		window.location.href = "/add-transaction"
+	}
+	
+	const addGroup = (event) => {
+		event.preventDefault()
+		window.location.href = "/add-group"
+	}
+	
+	const addFriend = (event) => {
+		event.preventDefault()
+		window.location.href = "/add-friend"
+	}
+	
+	const myActivity = (event) => {
+		event.preventDefault()
+		window.location.href = "/my-activities"
+	}
+
 	return (
 		<nav class="navbar navbar-dark bg-dark">
             <span className="navbar-brand h1 set-span-nav"><a href="/" className="navbar-link">ETET</a></span>
 			{
 			sessionStorage.getItem("userId") ?
-			<span className="navbar-brand h1 set-span-nav"><button className='btn btn-info' onClick={logout}>Logout</button></span>	: ""
+			<React.Fragment>
+				<span className="navbar-brand h1 set-span-nav-position">
+					<button className="btn btn-info" onClick={addFriend}>Add Friend</button>&nbsp;&nbsp;
+					<button className="btn btn-info" onClick={addTransaction}>Add Transaction</button>&nbsp;&nbsp;
+					<button className="btn btn-info" onClick={addGroup}>Add Group</button>&nbsp;&nbsp;
+					<button className="btn btn-info" onClick={myActivity}>My Activity</button>&nbsp;&nbsp;
+				</span>
+				<span className="navbar-brand h1 set-span-nav"><button className='btn btn-danger' onClick={logout}>Logout</button></span>
+			</React.Fragment> : ""
 		 	}
 			
         </nav>
--- server/app.js
+++ server/app.js
@@ -4,50 +4,17 @@ const app = express();
 const session = require('express-session');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
+const { OAuth2Client } = require("google-auth-library");
+const jwt = require("jsonwebtoken");
+require("dotenv/config"); //
 
-app.use(cors())
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-// const static = express.static(__dirname + '/public');
-// app.use('/public', static);
-
-// app.use(session({
-//     name: 'AuthCookie',
-//     secret: 'This cookie is used to determine login info',
-//     resave: false,
-//     saveUninitialized: true
-//   }));
-  
-
-// app.use('/login', (req,res, next) => {
-//     if(req.session.user){
-//         return res.redirect('/dashboard')
-//     }
-//     else{
-//         req.method = 'POST';
-//         next();
-//     }
-// })
-
-// app.use('/register', (req,res, next) => {
-//     if(req.session.user){
-//         return res.redirect('/dashboard')
-//     }
-//     else{
-//         next();
-//     }
-// })
-
-// app.use('/dashboard', (req,res, next) => {
-//     if(!req.session.user){
-//         return res.redirect('/')
-//     }
-//     else{
-//         next();
-//     }
-// })
 
 configRoutes(app);
 
